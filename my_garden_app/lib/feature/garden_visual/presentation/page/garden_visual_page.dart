f923160cb8c7b3085c31cecd77055d3b8be387e4
Timofey Latypov <90103780+N0tilT@users.noreply.github.com>
2024-07-28 22:41:53 +0300

    Update garden_visual_page.dart

--- my_garden_app/lib/feature/garden_visual/presentation/page/garden_visual_page.dart
+++ my_garden_app/lib/feature/garden_visual/presentation/page/garden_visual_page.dart
@@ -1,182 +1,259 @@
 import 'package:flutter/material.dart';
-class GardenVisualPage extends StatefulWidget {
-  
-  const GardenVisualPage({super.key});
 
+class GardenVisualPage extends StatefulWidget {
+  const GardenVisualPage();
   @override
   _GardenVisualPageState createState() => _GardenVisualPageState();
 }
 
 class _GardenVisualPageState extends State<GardenVisualPage> {
-  List<NoteData> notes = [];
+  List<RectangleItem> rectangles = [];
+  RectangleItem? currentRectangle;
+  Offset offset = Offset.zero;
+  static const double gridSize = 50.0;
 
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        showDialog(
-          context: context,
-          builder: (context) {
-            return AlertDialog(
-              title: const Text("Введите текст заметки"),
-              content: TextField(
-                onSubmitted: (text) {
-                  setState(() {
-                    notes.add(NoteData(position: const Offset(50, 50), text: text));
-                  });
-                  Navigator.of(context).pop();
-                },
-              ),
-            );
-          },
-        );
-      },
-      child: ColoredBox(
-        color: Colors.white,
-        child: Stack(
-          children: List.generate(notes.length, (index) {
-            return DraggableNote(
-              noteData: notes[index],
-              onDelete: () {
-                setState(() {
-                  notes.removeAt(index);
-                });
-              },
-              onEdit: (newText) {
-                setState(() {
-                  notes[index].text = newText;
-                });
-              },
-            );
-          }),),
-        ),
-      
-    );
+  void _addRectangle(RectangleType type) {
+    setState(() {
+      currentRectangle = RectangleItem(
+          type: type,
+          truePosition: Offset.zero + Offset(50.0 * (offset.dx ~/ 50),50.0 * (offset.dy ~/ 50)),
+          position: Offset.zero + Offset(50.0 * (offset.dx ~/ 50),50.0 * (offset.dy ~/ 50)),
+          isMoving: true);
+    });
   }
-}
 
-class NoteData {
-  Offset position;
-  String text;
-  double width;
-  double height;
+  void _confirmRectangle() {
+    if (currentRectangle != null) {
+      setState(() {
+        rectangles.add(currentRectangle!);
+        currentRectangle = null;
+      });
+    }
+  }
 
-  NoteData({
-    required this.position,
-    required this.text,
-    this.width = 100,
-    this.height = 50,
-  });
-}
+  void _toggleMovement(RectangleItem rectangle) {
+    setState(() {
+      rectangle.isMoving = !rectangle.isMoving;
+      if (rectangle.isMoving) {
+        currentRectangle = rectangle; // Устанавливаем текущий прямоугольник
+      } else {
+        currentRectangle = null; // Сбрасываем если движение остановлено
+      }
+    });
+  }
 
-class DraggableNote extends StatefulWidget {
-  final NoteData noteData;
-  final ValueChanged<String> onEdit;
-  final VoidCallback onDelete;
-
-  const DraggableNote({
-    super.key,
-    required this.noteData,
-    required this.onEdit,
-    required this.onDelete,
-  });
-
-  @override
-  _DraggableNoteState createState() => _DraggableNoteState();
-}
-
-class _DraggableNoteState extends State<DraggableNote> {
-  bool isMoving = false;
+  void _moveRectangle(Offset delta) {
+    if (currentRectangle != null) {
+      setState(() {
+        currentRectangle!.truePosition += delta;
+        final double deltaX = currentRectangle!.truePosition.dx.round() -
+            currentRectangle!.position.dx;
+        final double deltaY = currentRectangle!.truePosition.dy.round() -
+            currentRectangle!.position.dy;
+        if (deltaX >= 50) {
+          currentRectangle!.position = Offset(
+            currentRectangle!.position.dx + gridSize,
+            currentRectangle!.position.dy,
+          );
+        } else if (deltaX <= -50) {
+          currentRectangle!.position = Offset(
+            currentRectangle!.position.dx - gridSize,
+            currentRectangle!.position.dy,
+          );
+        }
+        if (deltaY >= 50) {
+          currentRectangle!.position = Offset(
+            currentRectangle!.position.dx,
+            currentRectangle!.position.dy + gridSize,
+          );
+        } else if (deltaY <= -50) {
+          currentRectangle!.position = Offset(
+            currentRectangle!.position.dx,
+            currentRectangle!.position.dy - gridSize,
+          );
+        }
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      left: widget.noteData.position.dx,
-      top: widget.noteData.position.dy,
-      child: GestureDetector(
-        onLongPress: () {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                title: const Text("Редактируйте заметку"),
-                content: TextField(
-                  controller: TextEditingController(text: widget.noteData.text),
-                  onSubmitted: (newText) {
-                    widget.onEdit(newText);
-                    Navigator.of(context).pop();
-                  },
-                ),
-              );
-            },
-          );
+    return Scaffold(
+      appBar: AppBar(title: const Text('Бесконечное клеточное поле')),
+      body: GestureDetector(
+        onPanUpdate: (details) {
+          print(details.delta);
+          if (currentRectangle != null && currentRectangle!.isMoving) {
+            print(currentRectangle!.position);
+            _moveRectangle(details.delta);
+          } else {
+            offset += details.delta;
+            setState(() {});
+            print(offset);
+          }
         },
-        onTap: () {
-          setState(() {
-            isMoving = !isMoving;
-          });
-        },
-        onPanUpdate: isMoving
-            ? (details) {
-                setState(() {
-                  widget.noteData.position += details.delta;
-                });
-              }
-            : null,
         child: Stack(
           children: [
-            Container(
-              width: widget.noteData.width,
-              height: widget.noteData.height,
-              decoration: BoxDecoration(
-                color: Colors.yellow,
-                border: Border.all(
-                  color: isMoving ? Colors.blue : Colors.transparent,
-                  width: 2,
+            Container(color: Colors.lightGreen[100]),
+            _buildGrid(),
+            ...rectangles.map((rectangle) {
+              return Positioned(
+                left: rectangle.position.dx + offset.dx,
+                top: rectangle.position.dy + offset.dy,
+                child: GestureDetector(
+                  onTap: () => _toggleMovement(rectangle),
+                  child: Container(
+                    width: rectangle.width,
+                    height: rectangle.height,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      border: Border.all(
+                        color: currentRectangle == rectangle
+                            ? Colors.blue
+                            : Colors.transparent,
+                        width: 2,
+                      ),
+                    ),
+                    child: Center(
+                        child: Text(rectangle.type.toString().split('.').last)),
+                  ),
+                ),
+              );
+            }),
+            if (currentRectangle != null)
+              Positioned(
+                left: currentRectangle!.position.dx + offset.dx,
+                top: currentRectangle!.position.dy + offset.dy,
+                child: Container(
+                  width: currentRectangle!.width,
+                  height: currentRectangle!.height,
+                  decoration: BoxDecoration(
+                    color: Colors.red.withOpacity(0.5),
+                    border: Border.all(color: Colors.blue, width: 2),
+                  ),
                 ),
               ),
-              child: Center(
-                child: Text(
-                  widget.noteData.text,
-                  style: const TextStyle(color: Colors.black),
-                ),
-              ),
-            ),
-            if (isMoving) _buildResizeHandle(1, 1),
           ],
         ),
       ),
-    );
-  }
-
-  Widget _buildResizeHandle(int x, int y) {
-    return Positioned(
-      left: x == 0 ? -40 : widget.noteData.width -40,
-      top: y == 0 ? -40 : widget.noteData.height -40,
-      child: GestureDetector(
-        onPanUpdate: (details) {
-          setState(() {
-            if (x == 0) {
-              widget.noteData.width -= details.delta.dx;
-            } else {
-              widget.noteData.width += details.delta.dx;
-            }
-            if (y == 0) {
-              widget.noteData.height -= details.delta.dy;
-            } else {
-              widget.noteData.height += details.delta.dy;
-            }
-
-            // Ограничение минимального размера
-            widget.noteData.width = widget.noteData.width.clamp(100.0, double.infinity);
-            widget.noteData.height = widget.noteData.height.clamp(100.0, double.infinity);
-          });
-        },
-        child: const CircleAvatar(
-          radius: 40,
-          backgroundColor: Colors.blue,
-        ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () => _showRectangleDialog(context),
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(width: 16),
+          FloatingActionButton(
+            onPressed: _confirmRectangle,
+            child: const Icon(Icons.check),
+          ),
+        ],
       ),
     );
   }
+
+  Widget _buildGrid() {
+    return CustomPaint(
+      size: Size.infinite,
+      painter: GridPainter(offset: offset),
+    );
+  }
+
+  void _showRectangleDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Выберите тип прямоугольника'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: RectangleType.values.map((RectangleType type) {
+              return ListTile(
+                title: Text(type.toString().split('.').last),
+                onTap: () {
+                  _addRectangle(type);
+                  Navigator.of(context).pop();
+                },
+              );
+            }).toList(),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class RectangleItem {
+  final RectangleType type;
+  Offset truePosition = Offset.zero;
+  Offset position = Offset.zero;
+  bool isMoving;
+
+  RectangleItem({
+    required this.truePosition,
+    required this.type,
+    required this.position,
+    this.isMoving = false,
+  });
+
+  double get width {
+    switch (type) {
+      case RectangleType.oneByOne:
+        return 50;
+      case RectangleType.twoByOne:
+        return 100;
+      case RectangleType.twoByThree:
+        return 100;
+      default:
+        return 50;
+    }
+  }
+
+  double get height {
+    switch (type) {
+      case RectangleType.oneByOne:
+        return 50;
+      case RectangleType.twoByOne:
+        return 50;
+      case RectangleType.twoByThree:
+        return 150;
+      default:
+        return 50;
+    }
+  }
+}
+
+enum RectangleType { oneByOne, twoByOne, twoByThree }
+
+class GridPainter extends CustomPainter {
+  Offset offset;
+
+  GridPainter({required this.offset});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.white
+      ..strokeWidth = 0.5;
+
+    for (double i = 0; i < size.width; i += 50) {
+      canvas.drawLine(
+        Offset(i + offset.dx, 0),
+        Offset(i + offset.dx, size.height),
+        paint,
+      );
+    }
+
+    for (double i = 0; i < size.height; i += 50) {
+      canvas.drawLine(
+        Offset(0, i + offset.dy),
+        Offset(size.width, i + offset.dy),
+        paint,
+      );
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 }
