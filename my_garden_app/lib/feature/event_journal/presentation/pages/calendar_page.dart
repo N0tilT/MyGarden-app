06295bd2e7784b75e2312db08eb4add374f4f4b8
Gromova Polina <90103780+N0tilT@users.noreply.github.com>
2025-04-26 23:18:18 +0300

    calendar page with multiple views

--- my_garden_app/lib/feature/event_journal/presentation/pages/calendar_page.dart
+++ my_garden_app/lib/feature/event_journal/presentation/pages/calendar_page.dart
@@ -10,8 +10,11 @@ import 'package:my_garden_app/feature/auth/presentation/bloc/cubit/token_cubit.d
 import 'package:my_garden_app/feature/event_journal/domain/entities/event_entity.dart';
 import 'package:my_garden_app/feature/event_journal/presentation/bloc/cubit/event_cubit.dart';
 import 'package:my_garden_app/feature/event_journal/presentation/widgets/event_bottom_sheet_widget.dart';
+import 'package:my_garden_app/feature/event_journal/presentation/widgets/event_list_widget.dart';
 import 'package:my_garden_app/feature/plant_list/presentation/bloc/plant_list/plant_list_cubit.dart';
 import 'package:my_garden_app/injection_container.dart';
+
+enum CalendarViewType { month, week, day, list }
 
 class CalendarPage extends StatefulWidget {
   const CalendarPage({super.key});
@@ -20,6 +23,30 @@ class CalendarPage extends StatefulWidget {
 }
 
 class _CalendarPageState extends State<CalendarPage> {
+  CalendarViewType _currentViewType = CalendarViewType.week;
+
+  void _switchViewType(CalendarViewType type) {
+    setState(() {
+      _currentViewType = type;
+    });
+  }
+
+  Widget _buildViewTypeTile(
+    BuildContext context,
+    String title,
+    IconData icon,
+    CalendarViewType type,
+  ) {
+    return ListTile(
+      leading: Icon(icon),
+      title: Text(title),
+      onTap: () {
+        _switchViewType(type);
+        Navigator.pop(context);
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
@@ -36,43 +63,89 @@ class _CalendarPageState extends State<CalendarPage> {
       ],
       child: Scaffold(
         appBar: const CalendarAppBarWidget(),
-        body: const _CalendarWidget(),
-        floatingActionButton: Builder(
-          builder: (parentContext) {
-            return FloatingActionButton(
-              onPressed: () {
-                showModalBottomSheet(
-                  context: parentContext,
-                  isScrollControlled: true,
-                  builder: (modal) => MultiBlocProvider(
-                    providers: [
-                      BlocProvider.value(
-                        value: BlocProvider.of<TokenCubit>(parentContext),
-                      ),
-                      BlocProvider.value(
-                        value: BlocProvider.of<EventCubit>(parentContext),
-                      ),
-                      BlocProvider.value(
-                        value: BlocProvider.of<PlantListCubit>(parentContext),
-                      ),
-                    ],
-                    child: EventCardBottomSheet(
-                      event: EventEntity(
-                        id: null,
-                        title: "",
-                        plantId: 0,
-                        date: DateTime.now(),
-                      ),
+        body: _CalendarWidget(currentViewType: _currentViewType),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              heroTag: 'view_switch',
+              onPressed: () => showModalBottomSheet(
+                context: context,
+                builder: (context) => Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    _buildViewTypeTile(
+                      context,
+                      'Месяц',
+                      Icons.calendar_month,
+                      CalendarViewType.month,
                     ),
+                    _buildViewTypeTile(
+                      context,
+                      'Неделя',
+                      Icons.calendar_view_week,
+                      CalendarViewType.week,
+                    ),
+                    _buildViewTypeTile(
+                      context,
+                      'День',
+                      Icons.calendar_today,
+                      CalendarViewType.day,
+                    ),
+                    _buildViewTypeTile(
+                      context,
+                      'Список',
+                      Icons.list,
+                      CalendarViewType.list,
+                    ),
+                  ],
+                ),
+              ),
+              child: const Icon(Icons.switch_left),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Builder(
+              builder: (parentContext) {
+                return FloatingActionButton(
+                  heroTag: 'add_event',
+                  onPressed: () {
+                    showModalBottomSheet(
+                      context: parentContext,
+                      isScrollControlled: true,
+                      builder: (modal) => MultiBlocProvider(
+                        providers: [
+                          BlocProvider.value(
+                            value: BlocProvider.of<TokenCubit>(parentContext),
+                          ),
+                          BlocProvider.value(
+                            value: BlocProvider.of<EventCubit>(parentContext),
+                          ),
+                          BlocProvider.value(
+                            value:
+                                BlocProvider.of<PlantListCubit>(parentContext),
+                          ),
+                        ],
+                        child: EventCardBottomSheet(
+                          event: EventEntity(
+                            id: null,
+                            title: "",
+                            plantId: 0,
+                            date: DateTime.now(),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    "+",
+                    style: TextStyle(fontSize: 20),
                   ),
                 );
               },
-              child: const Text(
-                "+",
-                style: TextStyle(fontSize: 20),
-              ),
-            );
-          },
+            ),
+          ],
         ),
       ),
     );
@@ -80,7 +153,8 @@ class _CalendarPageState extends State<CalendarPage> {
 }
 
 class _CalendarWidget extends StatelessWidget {
-  const _CalendarWidget();
+  final CalendarViewType currentViewType;
+  const _CalendarWidget({required this.currentViewType});
 
   @override
   Widget build(BuildContext context) {
@@ -111,7 +185,10 @@ class _CalendarWidget extends StatelessWidget {
               if (eventList.isEmpty) {
                 eventCubit.load();
               }
-              return _CalendarWrapper(eventList: eventList);
+              return _CalendarWrapper(
+                eventList: eventList,
+                currentViewType: currentViewType,
+              );
             },
             fail: (message) => Center(
               child: GardenDefaultLabelWidget(text: message, fontSize: 18),
@@ -128,6 +205,7 @@ class _CalendarWidget extends StatelessWidget {
               }
               return _CalendarWrapper(
                 eventList: eventList,
+                currentViewType: currentViewType,
               );
             },
           ),
@@ -137,7 +215,11 @@ class _CalendarWidget extends StatelessWidget {
 
 class _CalendarWrapper extends StatelessWidget {
   final List<EventEntity> eventList;
-  const _CalendarWrapper({required this.eventList});
+  final CalendarViewType currentViewType;
+  const _CalendarWrapper({
+    required this.eventList,
+    required this.currentViewType,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -151,42 +233,93 @@ class _CalendarWrapper extends StatelessWidget {
               )
               .toList(),
         );
-    return MonthView(
-      controller: CalendarControllerProvider.of(context).controller,
-      headerStringBuilder: (date, {secondaryDate}) {
-        final monthNames = [
-          "Январь",
-          "Февраль",
-          "Март",
-          "Апрель",
-          "Май",
-          "Июнь",
-          "Июль",
-          "Август",
-          "Сентябрь",
-          "Октябрь",
-          "Ноябрь",
-          "Декабрь",
-        ];
-        return "${monthNames[date.month - 1]} ${date.year}";
-      },
-      weekDayStringBuilder: (p0) {
-        final weekdays = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
-        return weekdays[p0];
-      },
-      minMonth: DateTime(1990),
-      maxMonth: DateTime(2050),
-      cellAspectRatio: 1,
-      onPageChange: (date, pageIndex) => print("$date, $pageIndex"),
-      onCellTap: (events, date) {
-        print(events);
-      },
-      onEventTap: (event, date) => print(event),
-      onEventDoubleTap: (events, date) => print(events),
-      onEventLongTap: (event, date) => print(event),
-      onDateLongPress: (date) => print(date),
-      hideDaysNotInMonth: true,
-    );
+
+    switch (currentViewType) {
+      case CalendarViewType.month:
+        return MonthView(
+          controller: CalendarControllerProvider.of(context).controller,
+          headerStringBuilder: (date, {secondaryDate}) {
+            final monthNames = [
+              "Январь",
+              "Февраль",
+              "Март",
+              "Апрель",
+              "Май",
+              "Июнь",
+              "Июль",
+              "Август",
+              "Сентябрь",
+              "Октябрь",
+              "Ноябрь",
+              "Декабрь",
+            ];
+            return "${monthNames[date.month - 1]} ${date.year}";
+          },
+          weekDayStringBuilder: (p0) {
+            final weekdays = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
+            return weekdays[p0];
+          },
+          minMonth: DateTime(1990),
+          maxMonth: DateTime(2050),
+          cellAspectRatio: 1,
+          onPageChange: (date, pageIndex) => print("$date, $pageIndex"),
+          onCellTap: (events, date) {
+            print(events);
+          },
+          onEventTap: (event, date) => print(event),
+          onEventDoubleTap: (events, date) => print(events),
+          onEventLongTap: (event, date) => print(event),
+          onDateLongPress: (date) => print(date),
+          hideDaysNotInMonth: true,
+        );
+      case CalendarViewType.week:
+        return WeekView(
+          controller: CalendarControllerProvider.of(context).controller,
+          headerStringBuilder: (date, {secondaryDate}) {
+            final monthNames = [
+              "Январь",
+              "Февраль",
+              "Март",
+              "Апрель",
+              "Май",
+              "Июнь",
+              "Июль",
+              "Август",
+              "Сентябрь",
+              "Октябрь",
+              "Ноябрь",
+              "Декабрь",
+            ];
+            return "${monthNames[date.month - 1]} ${date.year}";
+          },
+          weekDayStringBuilder: (p0) {
+            final weekdays = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
+            return weekdays[p0];
+          },
+          onPageChange: (date, pageIndex) => print("$date, $pageIndex"),
+          onEventTap: (event, date) => print(event),
+          onEventDoubleTap: (events, date) => print(events),
+          onEventLongTap: (event, date) => print(event),
+          onDateLongPress: (date) => print(date),
+        );
+      case CalendarViewType.day:
+        return DayView(
+          controller: CalendarControllerProvider.of(context).controller,
+          onPageChange: (date, pageIndex) => print("$date, $pageIndex"),
+          onEventTap: (event, date) => print(event),
+          onEventDoubleTap: (events, date) => print(events),
+          onEventLongTap: (event, date) => print(event),
+          onDateLongPress: (date) => print(date),
+        );
+      case CalendarViewType.list:
+        return Column(
+          children: eventList
+              .map(
+                (e) => EventListItem(event: e),
+              )
+              .toList(),
+        );
+    }
   }
 }
 
